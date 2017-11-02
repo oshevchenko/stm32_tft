860b3e209891778c574a1563dc2a25c80aee9356
Oleksandr Shevchenko <oleksandr.shevchenko@synapse.com>
2017-11-02 21:26:20 +0200

    First working version on CooCox IDE

---
+++ app/Src/stm32_microrl_misc.h
@@ -0,0 +1,29 @@
+#ifndef _STM32_MICRORL_MISC_H_
+#define _STM32_MICRORL_MISC_H_
+
+/*
+Platform independent interface for implementing some specific function
+for AVR, linux PC or ARM
+*/
+
+// init platform
+void init (void);
+
+// print to stream callback
+void print (const char * str);
+
+// get_char from stream
+//char get_char (void);
+
+// execute callback
+int execute (int argc, const char * const * argv);
+
+// completion callback
+char ** complet (int argc, const char * const * argv);
+
+// ctrl+c callback
+void sigint (void);
+
+void stm32_microrl_insert_char(int ch);
+void TERM_Task(void);
+#endif //_STM32_MICRORL_MISC_H_
